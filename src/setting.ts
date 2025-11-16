c749aa6fbf70624d90999e63e10722daab94a294
xffish <xffish@126.com>
2025-11-16 10:52:18 +0800

    fix issue #5 remove parameter of --resource-dir

--- src/setting.ts
+++ src/setting.ts
@@ -1,10 +1,7 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import * as vscode from 'vscode';
 
 interface Setting {
     executable: string,
-    resourceDir: string,
     mode: string,
     host: string,
     port: number,
@@ -14,21 +11,9 @@ export function getSetting(): Setting | undefined {
     const setting = vscode.workspace.getConfiguration('clice')
     const executable = setting.get<string>('executable');
     const mode = setting.get<string>('mode');
-    let config = setting.get<string>('config');
 
     if (!executable || executable === "") {
         vscode.window.showErrorMessage("The path of clice executable is not set, please set and restart the extension.");
-        return undefined
-    }
-
-    let resourceDir = path.resolve(path.dirname(executable), "..", "lib", "clang", "20")
-    if (!fs.existsSync(resourceDir)) {
-        /// FIXME: Currently clice is not located in bin, fix it in the future.
-        resourceDir = path.resolve(path.dirname(executable), "lib", "clang", "20")
-    }
-
-    if (!fs.existsSync(resourceDir)) {
-        vscode.window.showErrorMessage(`Unexpected error, resource dir not found: ${resourceDir}`);
         return undefined
     }
 
@@ -41,6 +26,6 @@ export function getSetting(): Setting | undefined {
     const port = setting.get<number>('port')!;
 
     return {
-        executable, resourceDir, mode, host, port,
+        executable, mode, host, port,
     }
 }
